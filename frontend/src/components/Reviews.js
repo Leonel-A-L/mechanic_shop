86bdf91f4c56bf23ca939b0871300df14d7fd85a
Josh Cline <joshccline@outlook.com>
2023-07-18 17:30:58 -0400

    Added reviews to the Reviews Page

--- frontend/src/components/Reviews.js
+++ frontend/src/components/Reviews.js
@@ -4,6 +4,43 @@ export default function Reviews() {
       <h1 className="reviewsHeader">
         Reviews
       </h1>
+      <div className="reviews">
+        <p>Carol B.  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span> : "The best mechanic shop I've ever been to. Affordable prices with top tier service!"
+        </p>
+        <p>Mike W. <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span> : "Great mechanic shop! Although a little pricey, you save more if you're a member."
+        </p>
+        <p>John A. <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span> : "Awesome car shop, services, and people!"
+        </p>
+        <p>Missy E. <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span> : "Got my vehicle repaired in no time! Very Fast, and very affordable!"
+        </p>
+        <p>Karen B. <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span> : "Very affordable and great service. Only had to ask for the manager once!"
+        </p>
+      </div>
     </div>
   )
 }
